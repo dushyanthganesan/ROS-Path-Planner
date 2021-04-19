b8696ce5005c54f28f7c164f8486172a32b5a480
Dushyanth Ganesan <dgn@umich.edu>
2021-04-19 00:13:46 -0400

    refactored packages

--- src/planning/src/astar.cpp
+++
@@ -1,5 +0,0 @@
-/* A-star path planning node to plan waypoints toward a goal-point 
-  navigating through obstacles */
-#include <iostream>
-#include <ros/ros.h>
-#include <visualization_msgs/Marker.h>
